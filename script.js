74093f634f3bf37ece44dfbced538e94369b6284
KChwistek <66901714+KChwistek-prog@users.noreply.github.com>
2021-01-17 12:03:45 +0100

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-   const apiRoot = 'https://boiling-tor-47300.herokuapp.com/v1/task/';
-   const trelloApiRoot = 'https://boiling-tor-47300.herokuapp.com/v1/trello/';
+   const apiRoot = 'https://boiling-tor-47300.herokuapp.com/v1/task';
+   const trelloApiRoot = 'https://boiling-tor-47300.herokuapp.com/v1/trello';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
 
