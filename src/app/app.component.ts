60978031ab21c6d25107ddf57d513a10eb0efd55
Krzysztof <k.chwistek@outlook.com>
2021-12-28 19:30:33 +0100

    code cleanup, working on calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title: string;
 
-  constructor() {
+  constructor(private http: HttpClient) {
   this.title = 'rPiMS-Front';
   }
+
+     sensors: Sensors;
+
+    public getReadings():Observable<any> {
+      return this.http.get('https://rpims-monitor.herokuapp.com/thermowelltmp/');
+    }
+
+    public gerCarbonationPressure():Observable<any>{
+      return this.http.get('https://rpims-monitor.herokuapp.com/carbonation/{temperature}&{desiredCarb}');
+    }
+
+    ngOnInit() {
+      this.getReadings().subscribe((res: Sensors) => this.sensors = res);
+    }
+}
+
+export interface Sensors {
+  thermowellTemperatureReadings: number;
+  roomTemperatureReadings: number;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RpimsSensorsListComponent } from './rpims-sensors-list/rpims-sensors-list.component';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    RpimsSensorsListComponent
+    AppComponent
   ],
   imports: [
     CommonModule,
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule
   ],
   providers: [],
